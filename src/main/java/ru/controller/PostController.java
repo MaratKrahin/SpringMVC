bc37d6b1ac6e70ccdd1906c3497a8ae2c10f7f84
Marat Krahin <paparasum@gmail.com>
2020-12-14 07:09:57 +0300

    springMVC hw done

---
+++ src/main/java/ru/controller/PostController.java
@@ -0,0 +1,37 @@
+package ru.controller;
+
+import ru.model.Post;
+import ru.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Map<Long, Post> all() {
+        return service.all();
+    }
+
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
+        service.removeById(id);
+    }
+}
